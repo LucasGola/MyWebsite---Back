c6d79fa486b5f532e4b6c454901b3d84d9775140
Lucas Silva <lusilva@goaheadit.com.br>
2022-09-06 12:43:31 -0300

    [add] utils (sendSuccess and sendError)

---
+++ controllers/util.js
@@ -0,0 +1,23 @@
+export const sendError = (res, err, status, message) => {
+    logToFile(err);
+    res.status(status || 500).json({
+        status: message,
+        err: err.message ? err.message : err.error,
+        timestamp: new Date(),
+    });
+};
+
+export const sendSuccess = (res, data, status, numberOfRows, message) => {
+    let qty = 0;
+    if (data && data.length) {
+        qty = data.length;
+        // eslint-disable-next-line no-param-reassign
+        if (!status) status = 200;
+    }
+    res.status(status || 200).json({
+        status: 'ok',
+        data,
+        timestamp: new Date(),
+        numberOfRows: numberOfRows || qty,
+    });
+};
